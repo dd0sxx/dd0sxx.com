2e720f9c7091c72e027aefb08db2dbe8b3f5775e
dd0sxx <contacttheotelonis@gmail.com>
2022-11-04 22:15:27 -0600

    messing w colors

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,22 +3,32 @@ import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
-    <>
+    <div class="bg-gradient-to-b from-[#4e6e9c] to-[#9eaffb] h-screen" >
       <Head>
-        <title>Fresh App</title>
+        <title>dd0sxx</title>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
+        <h1 class="my-2 mx-auto text-center text-[#01ccff] font-bold text-xl">
+          dd0sxx
+        </h1>
+        <p class="mt-4 mx-auto text-center text-[#01ccff] ">
+          Full Stack & Smart Contract Developer
+        </p>
+        <p class="mt-2 mb-6 mx-auto text-center text-[#01ccff] ">
+          Previous Core Contributor @ <a class="underline decoration-indigo-400" href="https://0xmacro.com/">0xMacro</a> & <a class="underline decoration-[]" href="https://uniwhales.io/">Uniwhales</a>
+        </p>
         <img
-          src="/logo.svg"
-          class="w-32 h-32"
+          src="/rei.png"
+          class="w-48 h-48 mx-auto"
           alt="the fresh logo: a sliced lemon dripping with juice"
         />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
+        <p class="mt-12 mb-6 mx-auto text-[#fbf6f1] text-sm">
+          <a class="underline decoration-[]" href="">
+            Alumni Soulbound Tokens - <span class="text-[#f89f94]">SBTs for Macro Engineering Fellowship Alum for on-chain proof of education</span>
+          </a>
         </p>
-        <Counter start={3} />
+        {/* <Counter start={3} /> */}
       </div>
-    </>
+    </div>
   );
 }
